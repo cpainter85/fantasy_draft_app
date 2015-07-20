c5c0f60ec1d54293b6513fb61dc11f37d3322601
Charles Painter <cpainter85@gmail.com>
2015-07-20 18:31:44 -0400

    updated game index spec

--- spec/features/game_index_spec.rb
+++ spec/features/game_index_spec.rb
@@ -1,17 +1,12 @@
 require 'rails_helper'
 
 feature 'Users can create new games, view existing games, and join existing games' do
-  let (:user) { create_user }
+  let (:user) { create(:user) }
 
   scenario 'a signed in user can create a new game' do
     user_sign_in(user)
-    new_game = {name: 'Comic Character Draft',
-                description: 'A fantasy draft of comic book characters'}
-    position_array = ["Lead Character (Drama)",
-                      "Lead Character (Comedy)",
-                      "Supporting Character (Drama)",
-                      "Supporting Character (Comedy)",
-                      "Cartoon Character"]
+    new_game = { name: Faker::Lorem.sentence, description: Faker::Lorem.paragraph }
+    position_array = (1..10).map { |x| Faker::Name.title }
 
     visit root_path
     click_link 'Create New Game'
@@ -22,14 +17,14 @@ feature 'Users can create new games, view existing games, and join existing game
     fill_in 'Description', with: new_game[:description]
 
     fill_in 'Positions', with: position_array.join("\n")
-    fill_in 'Team Name', with: 'The Champions'
+    fill_in 'Team Name', with: Faker::Team.name
     click_button 'Create Game'
 
     expect(current_path).to eq root_path
     expect(page).to have_content "#{new_game[:name]} successfully created!"
     expect(page).to have_content "#{new_game[:name]} (1 team) - #{position_array.length} Rounds"
     expect(Game.last.users).to eq([user])
-    expect(Game.last.positions.last.name).to eq('Cartoon Character')
+    expect(Game.last.positions.order(:id).pluck(:name)).to eq(position_array)
   end
 
   scenario 'a non-signed in user cannot create a new game' do
@@ -41,9 +36,10 @@ feature 'Users can create new games, view existing games, and join existing game
   end
 
   scenario 'a signed in user can join an existing game' do
-    game = create_game
-    team = create_team(game, user)
-    user2 = create_user(name: 'Captain America', email: 'sentinelofliberty@email.com')
+    game = create(:game)
+    team = create(:team, game: game, user: user)
+    user2 = create(:user)
+
     user_sign_in(user2)
 
     visit root_path
@@ -52,7 +48,7 @@ feature 'Users can create new games, view existing games, and join existing game
 
     expect(current_path).to eq new_game_team_path(game)
     expect(page).to have_content "Join #{game.name}"
-    fill_in 'Team Name', with: 'The Squadron Supreme'
+    fill_in 'Team Name', with: Faker::Team.name
     click_button 'Join Game'
 
     expect(page).to have_content "You have successfully joined #{game.name}!"
@@ -65,7 +61,7 @@ feature 'Users can create new games, view existing games, and join existing game
   end
 
   scenario 'a non-signed in user cannot join a game' do
-    game = create_game
+    create(:game)
 
     visit root_path
     click_link 'Join this game'
