683b3d5431ef783762e4302ab89fc9486caeff53
Charles Painter <cpainter85@gmail.com>
2015-07-20 18:44:09 -0400

    updated game show spec

--- spec/features/game_show_spec.rb
+++ spec/features/game_show_spec.rb
@@ -1,35 +1,25 @@
 require 'rails_helper'
 
 feature 'User can view picks in a game and draft new picks' do
-  let (:user) { create_user }
-  let! (:game) { create_game }
-  let! (:team) { create_team(game, user) }
-  let (:user2) { create_user(email: 'thor@email.com') }
-  let! (:team2) { create_team(game, user2, name: 'The Vikings') }
-  let! (:position) { create_position(game) }
-  let! (:position2) { create_position(game, name: 'Catchphrase') }
-  let! (:pick1) { create_pick(team, position) }
-  let! (:pick2) { create_pick(team2, position2) }
-
   scenario 'anyone can view game progress' do
+    game = create(:game)
+    teams = create_list(:team, 5, game: game)
+    positions = create_list(:position, 10, game: game)
+    round_1_picks = teams.map.with_index { |team, index| create(:pick, team: team, position: positions[index], round_drafted: 1) }
+    round_2_picks = teams.map.with_index { |team, index| create(:pick, team: team, position: positions[index+5], round_drafted: 2) }
+
     visit root_path
     click_link game.name
 
     expect(current_path).to eq game_path(game)
-    page_content = ['Round 1',
-                    'Round 2',
-                    game.name,
-                    team.name,
-                    team2.name,
-                    pick1.name,
-                    pick1.from,
-                    pick1.position.name,
-                    pick2.name,
-                    pick2.from,
-                    pick2.position.name]
+    expect(page).to have_content game.name
+    (1..positions.length).each { |round| expect(page).to have_content "Round #{round}" }
+    teams.each { |team| expect(page).to have_content team.name }
 
-    page_content.each do |content|
-      expect(page).to have_content content
+    round_1_picks.concat(round_2_picks).each do |pick|
+      expect(page).to have_content pick.name
+      expect(page).to have_content pick.from
+      expect(page).to have_content pick.position.name
     end
   end
 end
