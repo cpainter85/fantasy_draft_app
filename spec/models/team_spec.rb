5f8e2532efd01de88275b64699581a4b37daa3a2
Charles Painter <cpainter85@gmail.com>
2015-07-20 15:08:08 -0400

    updated team_spec.rb

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe Team do
-  let (:game) { create_game }
-  let (:user) { create_user }
-  let! (:team) { create_team(game, user) }
+  let(:game) { create(:game) }
+  let(:user) { create(:user) }
+  let!(:team) { create(:team, user: user, game: game) }
 
   describe 'associations' do
     describe '#game' do
@@ -17,19 +17,15 @@ describe Team do
       end
     end
     describe '#picks' do
-      let (:position) { create_position(game) }
-      let (:position2) { create_position(game, name: 'Lead in a Comedy') }
-      let (:pick) { create_pick(team, position) }
-      let (:pick2) { create_pick(team, position2, round_drafted: 2) }
-
       it 'returns the picks belonging to a team' do
-        expect(team.picks).to eq([pick, pick2])
+        picks = create_list(:pick, 10, team: team)
+        expect(team.picks.order(:id)).to eq(picks)
       end
     end
   end
 
   describe 'validations' do
-    it 'validates the presence of a team name' do
+    it 'must have a name' do
       team.update_attributes(name: nil)
       expect(team.errors.messages).to eq(name: ['can\'t be blank'])
     end
@@ -47,17 +43,17 @@ describe Team do
   describe 'methods' do
     describe '#next_draft' do
       it 'returns the number for the next available draft order number' do
-        new_team = Team.new(game_id: game.id, name: 'The Titans')
+        new_team = build(:team, game: game)
         new_team.next_draft
         new_team.save
 
-        expect(Team.last.draft_order).to eq(2)
+        expect(new_team.draft_order).to eq(2)
 
-        third_team = Team.new(game_id: game.id, name: 'Teen Titans')
+        third_team = build(:team, game: game)
         third_team.next_draft
         third_team.save
 
-        expect(Team.last.draft_order).to eq(3)
+        expect(third_team.draft_order).to eq(3)
       end
     end
   end
