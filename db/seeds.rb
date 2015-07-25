71f6c77cb0717a8f14b4b13900a1f5dcadf68755
Charles Painter <cpainter85@gmail.com>
2015-07-25 14:45:00 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+Game.destroy_all
+User.destroy_all
+Team.destroy_all
+Position.destroy_all
+Pick.destroy_all
+
+game = Game.create(name: Faker::Lorem.sentence, description: Faker::Lorem.paragraph)
+user = User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 'password1234')
+team = Team.create(game: game, user: user, name: Faker::Team.name, draft_order: 1)
+other_teams = (1..5).each { |number| Team.create(game: game, user: user, name: Faker::Team.name, draft_order: number+1) }
+positions = (1..15).map { Position.create(game: game, name: Faker::Name.title) }
+round_1_picks = game.teams.each_with_index {|team, index| Pick.create(team: team, position: positions[index], name: Faker::Name.name, from: Faker::Company.name, round_drafted: 1)}
+round_2_picks = game.teams.each_with_index {|team, index| Pick.create(team: team, position: positions[index+1], name: Faker::Name.name, from: Faker::Company.name, round_drafted: 2)}
